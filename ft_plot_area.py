ed4d247d2e9d163d22b26dab7d1cb6c088d7696c
o.robert1994@hotmail.com <rokuni@f0r4s26.codam.nl>
2026-05-01 16:30:32 +0200

    send

---
+++ ft_plot_area.py
@@ -0,0 +1,9 @@
+def ft_plot_area():
+	length = int(input("Enter length: "))
+	widith = int(input("Enter width: "))
+	results = length * widith
+
+	print(f"Plot area: {results}")
+
+if __name__=="__main__":
+	ft_plot_area()
