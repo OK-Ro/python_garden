a7697f3e3b0f50b2b89581d5f3e1fd928fbab352
o.robert1994@hotmail.com <rokuni@f1r4s14.codam.nl>
2026-05-06 13:53:26 +0200

    new

--- module02/ex4/ft_finally_block.py
+++ module02/ex4/ft_finally_block.py
@@ -1,14 +1,67 @@
 class GardenError(Exception):
-    def __init__(self, message = "Unknown garden error"):
+    def __init__(self, message="Unknown garden error"):
         super().__init__(message)
+
+
+class PlantError(GardenError):
+    def __init__(self, message="Unknown plant error"):
+        super().__init__(message)
+
+
+class WaterError(GardenError):
+    def __init__(self, message="Unknown water error"):
+        super().__init__(message)
+
 
 def water_plant(plant_name):
     capitalized = plant_name.capitalize()
+
     if plant_name != capitalized:
-        print("Not capilized")
-        # raise PlantError("Caught PlantError:")
-    else:
-        print(f"Watering {capitalized}: [OK]")
-        
+        raise PlantError(f"Invalid plant name to water: '{plant_name}'")
+
+    print(f"Watering {capitalized}: [OK]")
+
+
+def test_watering_system():
+    print("=== Garden Watering System ===")
+    print()
+
+    # ---------------- VALID TEST ----------------
+    print("Testing valid plants...")
+    print("Opening watering system")
+
+    try:
+        water_plant("Tomato")
+        water_plant("Lettuce")
+        water_plant("Carrots")
+
+    except PlantError as error:
+        print(f"Caught PlantError: {error}")
+
+    finally:
+        print("Closing watering system")
+        print()
+
+    # ---------------- INVALID TEST ----------------
+    print("Testing invalid plants...")
+    print("Opening watering system")
+
+    try:
+        water_plant("tomato")
+        water_plant("lettuce")
+        water_plant("carrots")
+
+    except PlantError as error:
+        print(f"Caught PlantError: {error}")
+        print(".. ending tests and returning to main")
+        return
+
+    finally:
+        print("Closing watering system")
+        print()
+
+    print("Cleanup always happens, even with errors!")
+
+
 if __name__ == "__main__":
-    water_plant("Banana")
+    test_watering_system()
